8a01f7028684484cbb0226261f8f98f7f6dc0c26
Rogerio <rogerioa.sobrinho@gmail.com>
2021-03-29 17:00:56 +0100

    feature: create datasource

---
+++ lib/modules/home/external/datasource/list_things_datasource_impl.dart
@@ -0,0 +1,10 @@
+import 'package:borrowed_things/modules/home/infra/datasource/list_things_datasource.dart';
+import 'package:borrowed_things/modules/home/infra/models/list_things_model.dart';
+
+class ListThingsDataSourceImpl implements ListThingsDataSource {
+  @override
+  Future<List<ListThingsModel>> getAll() {
+    // TODO: implement getAll
+    throw UnimplementedError();
+  }
+}
